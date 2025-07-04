ec9b671c54a7c810dbaccaa92fc6715124681357
island127 <194112325@qq.com>
2025-07-04 21:31:46 +0800

    吸顶导航交互事件

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,10 +1,12 @@
 <script setup>
+import LayoutFixed from './components/LayoutFixed.vue'
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
 <template>
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
