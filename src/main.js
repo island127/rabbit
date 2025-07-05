1a55a575ea0204d9777bdf1f05440b85169da0bb
island127 <194112325@qq.com>
2025-07-05 12:26:23 +0800

    懒加载指令封装

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useIntersectionObserver } from '@vueuse/core'
 
 import App from './App.vue'
 import router from './router'
@@ -14,3 +15,21 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    // el：指令绑定的那个元素 img
+    // binding:binding.value  指令等于号后面绑定的表达式的值 图片url
+    console.log(el, binding.value);
+    useIntersectionObserver(
+      el,
+      ([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          // 进入视口区域
+          el.src = binding.value
+        }
+      },
+    )
+  }
+})
