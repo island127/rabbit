4e95d44aa4f5946aa788fe3e8b9039114c447861
island127 <194112325@qq.com>
2025-07-05 16:23:38 +0800

    完成Product列表

--- src/apis/home.js
+++ src/apis/home.js
@@ -20,12 +20,23 @@ export const findNewAPI = () => {
 }
 
 /**
- * @description: 获取热门好物
+ * @description: 获取人气推荐
  * @param {*}
  * @return {*}
  */
 export const findHotAPI = () => {
   return httpInstance({
     url: '/home/hot'
+  })
+}
+
+/**
+ * @description: 获取所有商品模块
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () => {
+  return httpInstance({
+    url: '/home/goods'
   })
 }
