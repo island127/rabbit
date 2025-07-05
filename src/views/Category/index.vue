fabb23ac40c997c891ab47ff93529f57210bef0a
island127 <194112325@qq.com>
2025-07-05 18:50:58 +0800

    home-轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { getCategoryAPI } from '@/apis/category.js'
+import { getBannerAPI } from '@/apis/home.js'
 import { ref, onMounted, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
@@ -21,6 +22,19 @@ watch(
   },
   { immediate: false } // 不需要立即执行，因为 onMounted 已经处理了初始加载
 )
+
+// 获取banner
+const bannerList = ref([])
+
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  })
+  bannerList.value = res.result
+}
+onMounted(() => {
+  getBanner()
+})
 </script>
 
 <template>
@@ -33,12 +47,33 @@ watch(
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+        <el-carousel height="500px">
+          <el-carousel-item v-for="item in bannerList" :key="item.id">
+            <img :src="item.imgUrl" alt="">
+          </el-carousel-item>
+        </el-carousel>
+      </div>
     </div>
   </div>
 </template>
 
 
 <style scoped lang="scss">
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+  left: 0;
+  top: 0;
+
+  img {
+    width: 100%;
+    height: 500px;
+  }
+}
+
 .top-category {
   h3 {
     font-size: 28px;
