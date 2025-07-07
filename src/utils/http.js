d57506ecb6b2a15637c587577b62333924cd4fbf
island127 <194112325@qq.com>
2025-07-07 14:55:14 +0800

    登陆页面-401token处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,6 @@
 // axios封装——基地址
 import axios from 'axios'
+import router from '@/router'
 import { useUserStore } from '@/stores/user'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
@@ -25,11 +26,19 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore = useUserStore()
   // 统一错误提示
   ElMessage({
     type: 'warning',
     message: e.response.data.message
   })
+  // 401token失效处理
+  // 1.清除本地用户数据
+  // 2.跳转到登录页
+  if (e.response.status === 401) {
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
