238f1ff12d5fee8d9c7ea55263437879be995b6d
island127 <194112325@qq.com>
2025-07-07 17:07:19 +0800

    购物车-本地头部购物车

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,7 +1,7 @@
 // 封装购物车模块
 
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 export const useCartStore = defineStore('cart', () => {
   // 1.定义state - cartList
@@ -12,7 +12,7 @@ export const useCartStore = defineStore('cart', () => {
     // 已添加过 - count + 1
     // 没有添加过 -直接push
     // 思路：通过匹配传递过来的商品对象中的skuId能不能再cartList中找到，找到了就是添加过
-    const item = cartList.value.find(() => {
+    const item = cartList.value.find((item) => {
       goods.skuId === item.skuId
     })
     if (item) {
@@ -29,8 +29,15 @@ export const useCartStore = defineStore('cart', () => {
     const idx = cartList.value.findIndex((item) => skuId === item.skuId)
     cartList.value.splice(idx, 1)
   }
+  // 计算属性
+  // 1.总的数量 所有项的count 之和
+  const allCount = computed(() => cartList.value.reduce((a, c) => a + c.count, 0))
+  // 2.总价 所有项的 count*price之和
+  const allPrice = computed(() => cartList.value.reduce((a, c) => a + c.count * c.price, 0))
   return {
     cartList,
+    allCount,
+    allPrice,
     addCart,
     delCart
   }
